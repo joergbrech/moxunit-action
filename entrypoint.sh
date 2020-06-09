7bc9b9976eeaa3ceebe2891decddb7b37d041c0b
Agah <agahkarakuzu@gmail.com>
2020-06-09 09:40:04 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,9 +39,10 @@ fi
 if [ -z $DATA ] ; then
   DATA=""
 else
-   SETUP="$SETUP addpath($PWD/$DATA);"
+   SETUP="$SETUP addpath('$PWD/$DATA');"
    echo "TEST DATA DIR: $PWD/$DATA"
    ls $PWD/$DATA
+   pkg list
 fi
 
 ###########################
